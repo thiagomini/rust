870985f8de85eaf0b0f5fab063301981ea9972b5
Thiago Martins <thiago_valentim1@hotmail.com>
2023-09-29 17:03:55 -0300

    feat(guesser): compare guessed value

--- guesser/src/main.rs
+++ guesser/src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     let mut guess = String::new();
 
@@ -11,4 +14,12 @@ fn main() {
         .expect("Failed to read line!");
 
     println!("You guessed: {}", guess);
+
+    let parsed_guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    if parsed_guess == secret_number {
+        println!("You win!");
+    } else {
+        println!("You lose! The number was {}.", secret_number);
+    }
 }
