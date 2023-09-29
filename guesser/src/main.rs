7425238b90421e57d606973c6f961dc821bc39c7
Thiago Martins <thiago_valentim1@hotmail.com>
2023-09-29 17:32:22 -0300

    feat(guesser): add loop

--- guesser/src/main.rs
+++ guesser/src/main.rs
@@ -1,25 +1,29 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Guess the number!");
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    let parsed_guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    if parsed_guess == secret_number {
-        println!("You win!");
-    } else {
-        println!("You lose! The number was {}.", secret_number);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("Too small!"),
+            Ordering::Greater => print!("Too big!"),
+            Ordering::Equal => {
+                print!("You won!");
+                break;
+            }
+        }
     }
 }
