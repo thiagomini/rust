55eccdb3de6401fe592c51ef18833440a6ac6d58
Thiago Martins <thiago_valentim1@hotmail.com>
2023-09-29 21:25:23 -0300

    feat(basic-rpg): attack and defense equal

--- basic-rpg/src/damage_calculator.rs
+++ basic-rpg/src/damage_calculator.rs
@@ -1,5 +1,5 @@
 fn calculate_damage(source_attack: u32, target_def: u32) -> u32 {
-  return 1;
+    return 1;
 }
 
 #[cfg(test)]
@@ -9,5 +9,10 @@ mod tests {
     #[test]
     fn deals_minimum_damage() {
         assert_eq!(1, calculate_damage(1, 2));
+    }
+
+    #[test]
+    fn damage_is_one_when_attack_and_defense_are_equal() {
+        assert_eq!(1, calculate_damage(1, 1));
     }
 }
