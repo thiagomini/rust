fd64dceb9a9916554f8c27830f66ef5d99b7dc96
Thiago Martins <thiago_valentim1@hotmail.com>
2023-10-01 14:11:06 -0300

    feat(basic-rpg): damage is attack minus defense

--- basic-rpg/src/damage_calculator.rs
+++ basic-rpg/src/damage_calculator.rs
@@ -1,5 +1,9 @@
+use std::cmp;
+
 fn calculate_damage(source_attack: u32, target_def: u32) -> u32 {
-    return 1;
+    let unbounded_damage = source_attack.checked_sub(target_def);
+    let safe_unsigned_result = unbounded_damage.unwrap_or(0);
+    return cmp::max(safe_unsigned_result, 1);
 }
 
 #[cfg(test)]
@@ -14,5 +18,10 @@ mod tests {
     #[test]
     fn damage_is_one_when_attack_and_defense_are_equal() {
         assert_eq!(1, calculate_damage(1, 1));
+    }
+
+    #[test]
+    fn damage_is_attack_minus_defense() {
+        assert_eq!(2, calculate_damage(3, 1));
     }
 }
